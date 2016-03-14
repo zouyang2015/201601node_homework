a5360fe68f3c6c07a00e6caf197c5e1c142aaee5
tangyumeng <tangyumeng@gmail.com>
2016-03-14 17:17:08 +0800

    第二周作业

--- 练习/唐余猛/第二周作业/第二题/realServer.js
+++ 练习/唐余猛/第二周作业/第二题/realServer.js
@@ -1,7 +1,13 @@
 var http = require('http');
 var url = require('url');
+var bodyParser = require('./bodyParser');
 
 var server = http.createServer(function (req,res) {
+    bodyParser(req, function (result) {
+        console.log('hhhhh');
+        console.log(result);
+        console.log('hhhhh');
+    });
     res.end('real server worked');
 });
 
--- 练习/唐余猛/第二周作业/第二题/simpleProxyServer.js
+++ 练习/唐余猛/第二周作业/第二题/simpleProxyServer.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var proxy = require('./request');
 var url = require('url');
-
+var bodyParser = require('./bodyParser');
 // 获取请求的headers，去掉host和connection
 var getHeader = function (req) {
     var ret = {};
@@ -12,21 +12,39 @@ var getHeader = function (req) {
     }
     return ret;
 };
+//暂时只支持get，post
 http.createServer(function (req, res) {
-    console.log(req.headers);
-
-    var path = url.parse(req.url).path;
-    var method = req.method;
-
-    var options = {
-        host:req.headers.realhost? req.headers.realhost : 'localhost',
-        port:req.headers.port ?req.headers.port : 8888,
-        path: path,
-        method: method,
-        headers:  getHeader(req)
+    switch(req.method.toLocaleLowerCase()){
+        case 'get':
+            var path = url.parse(req.url).path;
+            var method = req.method;
+            var options = {
+                host:req.headers.realhost? req.headers.realhost : 'localhost',
+                port:req.headers.port ?req.headers.port : 8888,
+                path: path,
+                method: method,
+                headers:  getHeader(req)
+            }
+            proxy(options, function (response) {
+                res.end(response);
+            });
+        break;
+        case 'post':
+            bodyParser(req, function (result) {
+                var options = {
+                    host: 'localhost',
+                    port: 8888,
+                    path: path,
+                    method: method,
+                    headers:  getHeader(req),
+                    body: result
+                }
+                proxy(options,result, function (response) {
+                    res.end(response);
+                });
+            });
+        break;
     }
-    proxy(options, function (response) {
-        res.end(response);
-    });
+    
 }).listen(9090);
 
